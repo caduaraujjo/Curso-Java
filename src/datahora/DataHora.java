9c844a6098dcf2118da6fa4020c532062b34f270
Cadu <caduaraujo1609@gmail.com>
2025-12-21 15:59:00 -0300

    Convertendo data-hora global para local

--- src/datahora/DataHora.java
+++ src/datahora/DataHora.java
@@ -51,6 +51,26 @@ public class DataHora {
 		System.out.println("d09 = " + d09);
 		System.out.println("d10 = " + d10);
 		System.out.println("d11 = " + d11);
+		
+		//Convertendo data-hora global para local
+		LocalDate r1 = LocalDate.ofInstant(d06, ZoneId.systemDefault());
+		LocalDate r2 = LocalDate.ofInstant(d06, ZoneId.of("Portugal"));
+		LocalDateTime r3 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
+		LocalDateTime r4 = LocalDateTime.ofInstant(d06, ZoneId.of("Portugal"));
+		
+		System.out.println("r1 = " + r1);
+		System.out.println("r2 = " + r2);
+		System.out.println("r3 = " + r3);
+		System.out.println("r4 = " + r4);
+		
+		System.out.println("d04 dia = " + d04.getDayOfMonth());
+		System.out.println("d04 mês = " + d04.getMonthValue());
+		System.out.println("d04 ano = " + d04.getYear());
+		
+		System.out.println("d05 hora = " + d05.getHour());
+		System.out.println("d05 minutos = " + d05.getMinute());
+		System.out.println("d05 segundos = " + d05.getSecond());
+		
 	}
 
 }
