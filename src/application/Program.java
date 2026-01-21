8025729b333f201291ecb127b546f937a90b4c9a
Cadu <caduaraujo1609@gmail.com>
2026-01-20 21:41:53 -0300

    FileWriter e BufferedWriter

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,24 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program {
 	public static void main(String[] args) {
 
-		String path = "D:\\\\Backup Cadu\\\\Desktop\\\\in.txt";
+		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night"};
 
-		try (BufferedReader br = new BufferedReader(new FileReader(path))){
-			String line = br.readLine();
+		String path = "D:\\\\Backup Cadu\\\\Desktop\\\\out.txt";
 
-			while (line != null) {
-				System.out.println(line);
-				line = br.readLine();
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
 			}
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
-		} 
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
