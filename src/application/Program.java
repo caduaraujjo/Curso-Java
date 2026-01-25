c7d7f074e4a22c49e577b4723bb2318141bd5554
Cadu <caduaraujo1609@gmail.com>
2026-01-25 19:03:00 -0300

    Exercício de fixação trabalhando com arquivos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,66 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Product;
 
 public class Program {
 	public static void main(String[] args) {
 
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter a folder path: ");
+		List<Product> list = new ArrayList<>();
+
+		System.out.println("Enter file path: ");
 		String strPath = sc.nextLine();
 
-		File path = new File(strPath);
+		File sourceFile = new File(strPath);
+		String source = sourceFile.getParent();
 
-		System.out.println("getPath: " + path.getPath());
-		System.out.println("getParent: " + path.getParent());
-		System.out.println("getName: " + path.getName());
+		boolean success = new File(source + "\\out").mkdir();
+		System.out.println("Folder created: " + success);
 
+		String targetFileStr = source + "\\out\\summary.csv";
+
+		try(BufferedReader br = new BufferedReader(new FileReader(strPath))) {
+
+			String itemCsv = br.readLine();
+			while (itemCsv != null) {
+
+				String[] fields = itemCsv.split(",");
+				String name = fields[0];
+				double price = Double.parseDouble(fields[1]);
+				int quantity = Integer.parseInt(fields[2]);
+
+				list.add(new Product(name, price, quantity));
+
+				itemCsv = br.readLine();
+			}
+		} catch (IOException e) {
+			System.out.println("Error writing file: " + e.getMessage());
+		}
+
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))) {
+
+			for (Product item: list) {
+				bw.write(item.getName() + "," + String.format("%.2f", item.total()));
+				bw.newLine();
+			}
+
+			System.out.println(targetFileStr + " CREATED");
+		} catch (IOException e) {
+			System.out.println("Error wiriting file: " + e.getMessage());
+		}
 		sc.close();
 	}
 }
