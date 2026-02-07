f79e28aa23d5375627bac8f75e9cb696b7963ff3
Cadu <caduaraujo1609@gmail.com>
2026-02-07 19:09:06 -0300

    Herdar vs. cumprir contrato

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,42 +1,20 @@
 package application;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
-import java.util.Scanner;
-
-import model.entities.Contract;
-import model.entities.Installment;
-import model.services.ContractService;
-import model.services.PaypalService;
+import model.entities.AbstractShape;
+import model.entities.Circle;
+import model.entities.Rectangle;
+import model.enums.Color;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Entre os dados do contrato:");
-		System.out.print("Numero: ");
-		Integer number = sc.nextInt();
-		System.out.print("Data (dd/MM/yyyy):");
-		LocalDate date = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-		System.out.print("Valor do contrato: ");
-		Double totalValue = sc.nextDouble();
-
-		Contract contract = new Contract(number, date, totalValue);
-
-		System.out.print("Entre com o numero de parcelas: ");
-		Integer months = sc.nextInt();
-
-		ContractService service = new ContractService(new PaypalService());
-		service.processContract(contract, months);
-
-		System.out.println();
-		System.out.println("Parcelas: ");
-		for (Installment i : contract.getInstallments()) {
-			System.out.println(i);
-		}
-		sc.close();
+		AbstractShape s1 = new Circle(Color.BLACK, 2.0);
+		AbstractShape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
+		
+		System.out.println("Circle color: " + s1.getColor());
+		System.out.println("Circle area: " + String.format("%.3f", s1.area()));
+		System.out.println("Rectangle color: " + s2.getColor());
+		System.out.println("Rectangle area: " + String.format("%.3f", s2.area()));
 	}
 }
