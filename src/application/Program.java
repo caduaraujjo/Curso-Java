cc4cc6c7f0029d95dc72d3d0729438d1b766fbcc
Cadu <caduaraujo1609@gmail.com>
2026-02-20 19:15:08 -0300

    Exercício resolvido (Set)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,34 +1,44 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+import entities.LogEntry;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
-		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
-		List<Object> myObjs = new ArrayList<Object>();
+		Scanner sc = new Scanner(System.in);
 
-		copy(myInts, myObjs);
-		print(myObjs);
-		copy(myDoubles, myObjs);
-		print(myObjs);
+		System.out.println("Enter file full path: ");
+		String path = sc.nextLine();
 
-	}
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
+			Set<LogEntry> set = new HashSet<>();
 
-	public static void copy(List<? extends Number> source, List<? super Number> destiny) {
-		for (Number number : source) {
-			destiny.add(number);
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(" ");
+				String username = fields[0];
+				Date moment = Date.from(Instant.parse(fields[1]));
+
+				set.add(new LogEntry(username, moment));
+
+				line = br.readLine();
+			}
+			System.out.println("Total users: " + set.size());
 		}
-	}
-
-	public static void print(List<? super Number> list) {
-		for (Object obj : list) {
-			System.out.print(obj + " ");
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
 		}
-		System.out.println();
+		sc.close();
 	}
 }
